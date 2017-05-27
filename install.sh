99fb03dc177cc5307d078c8bc15d7836435f71a0
Braindead Security <braindead-sec@users.noreply.github.com>
2017-05-26 22:31:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
-
+#!/bin/sh -e
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root." 1>&2
+   exit 1
+fi
+# Install dependencies
 apt-get install macchanger hostapd dnsmasq apache2 php5
+# Configure components
 cp -f hostapd.conf /etc/hostapd/
 cp -f dnsmasq.conf /etc/
 cp -f dnsmasq.hosts /etc/
 rm -f /var/www/html/index.html
 cp -f html/* /var/www/html/
 cp -f rc.local /etc/
-echo "Rogue captive portal installed. Reboot to execute.\n"
+# All done
+echo "Rogue captive portal installed. Reboot to execute."
+exit 0
